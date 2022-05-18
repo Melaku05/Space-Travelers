01a57ecd75222777f7b64387318006f9a499782f
melaku05 <melakushiferaw05@gmail.com>
2022-05-18 17:25:10 +0300

    linter error

--- src/App.js
+++ src/App.js
@@ -4,14 +4,16 @@ import routes from './routes';
 
 function App() {
   return (
-    <div className="App">
-      <Navbar />
-      <Routes>
-        { routes.map((route) => (
-          <Route path={route.path} element={route.element} key={route.id} />
-        ))}
-      </Routes>
-    </div>
+    <>
+      <div className="App">
+        <Navbar />
+        <Routes>
+          { routes.map((route) => (
+            <Route path={route.path} element={route.element} key={route.id} />
+          ))}
+        </Routes>
+      </div>
+    </>
   );
 }
 
--- src/components/Nav.js
+++ src/components/Nav.js
@@ -4,25 +4,28 @@ import routes from '../routes';
 import './Nav.css';
 
 const Nav = () => (
-  <header className="contained">
-    <div className="logo-title">
-      <img src={logo} alt="Logo" />
-      <h1>Space Travelers Hubs  </h1>
-    </div>
-    <nav>
-      <ul className="menu-list">
-        {routes.map((route) => (
-          <li key={route.id}>
-            <NavLink
-              className={({ isActive }) => (isActive ? 'menu-item active' : 'menu-item')}
-              to={route.path}
-            >
-              {route.title}
-            </NavLink>
-          </li>
-        ))}
-      </ul>
-    </nav>
-  </header>
+  <>
+    <header className="contained">
+      <div className="logo-title">
+        <img src={logo} alt="Logo" />
+        <h1>Space Travelers Hubs </h1>
+      </div>
+      <nav>
+        <ul className="menu-list">
+          {routes.map((route) => (
+            <li key={route.id}>
+              <NavLink
+                className={({ isActive }) => (isActive ? 'menu-item active' : 'menu-item')}
+                to={route.path}
+              >
+                {route.title}
+              </NavLink>
+            </li>
+          ))}
+        </ul>
+      </nav>
+    </header>
+  </>
 );
+
 export default Nav;
